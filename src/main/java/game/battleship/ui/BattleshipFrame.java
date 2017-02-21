73e1fdc7335ba6a00d9936f996cc3a1b82419581
Ben Oeyen <ben.oeyen@gmail.com>
2017-02-21 08:43:20 +0100

    Added cover

--- src/main/java/game/battleship/ui/BattleshipFrame.java
+++ src/main/java/game/battleship/ui/BattleshipFrame.java
@@ -2,14 +2,10 @@ package game.battleship.ui;
 
 import game.battleship.model.GameState;
 import game.battleship.model.Player;
-import game.battleship.model.Sea;
-import game.battleship.service.FiringService;
 import game.battleship.service.VictoryService;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 /**
  * Developer: Ben Oeyen
@@ -19,26 +15,39 @@ public class BattleshipFrame extends JFrame {
 
     public static final boolean DEBUG = true;
 
+    private static final String COVER = "src/main/resources/image/cover.jpeg";
+
     private static BattleshipFrame frame;
 
-    public static BattleshipFrame getInstance(){
-        if(frame == null){
+    public static BattleshipFrame getInstance() {
+        if (frame == null) {
             frame = new BattleshipFrame();
         }
         return frame;
     }
 
-    private BattleshipFrame(){
+    private BattleshipFrame() {
         super("Battleship");
     }
 
-    public void initFrame(){
+    public void initFrame() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setJMenuBar(new CustomMenuBar());
+        showCover();
     }
 
-    public void showState(){
+    public void showCover() {
+        ImageIcon image = new ImageIcon(COVER);
+        JLabel label = new JLabel("", image, JLabel.CENTER);
+        JPanel jPanel = new JPanel();
+        jPanel.add(label);
+        setContentPane(jPanel);
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+    public void showState() {
         JPanel jPanel = new JPanel(new BorderLayout(0, 0));
         jPanel.add(new PlayerSidePanel(GameState.getInstance().getP1(), GameState.getInstance().getSea1(), GameState.getInstance().getSea2(), Color.GREEN), BorderLayout.WEST);
         jPanel.add(new PlayerSidePanel(GameState.getInstance().getP2(), GameState.getInstance().getSea2(), GameState.getInstance().getSea1(), Color.RED), BorderLayout.EAST);
